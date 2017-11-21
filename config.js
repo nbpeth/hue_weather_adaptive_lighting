08f161a23a20a68c516b2644928e41501ceb5aeb
bud manstrong <nbpeth@gmail.com>
2017-11-21 12:25:35 -0600

    pull lat long into env var

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const vars = {
 	dark_sky_key: process.env.DARK_SKY_KEY,
 	dark_sky_base_url:'https://api.darksky.net/forecast/',
-	lat_long:'38.9546842,-92.4028331',
+	lat_long:process.env.LAT_LONG,
 	hue_user:process.env.HUE_USER,
 	hue_url:process.env.HUE_URL
 
